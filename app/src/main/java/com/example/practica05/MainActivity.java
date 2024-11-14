fc896701e7af1fe99056eecfb96479856d1a6336
Pau Pedros <paupedros10@gmail.com>
2024-11-14 19:31:02 +0100

    added dropdown and intent to main

--- app/src/main/java/com/example/practica05/MainActivity.java
+++ app/src/main/java/com/example/practica05/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.practica05;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +24,27 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Referencia al Spinner
+        Spinner dropdownMenu = findViewById(R.id.dropdown_menu);
+
+        // Carga el array de strings desde los recursos
+        String[] restaurants = getResources().getStringArray(R.array.restaurants);
+
+        // Configura el adaptador para el Spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, restaurants);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        dropdownMenu.setAdapter(adapter);
+
+        Button demana = findViewById(R.id.demanaBtn);
+        demana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Activity_Hamburguesas.class);
+                startActivity(intent);
+            }
         });
     }
 }
