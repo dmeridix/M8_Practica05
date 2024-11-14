4f1c93f53d45134ed146d794c0be509db1d981f0
Pau Pedros <paupedros10@gmail.com>
2024-11-14 19:41:59 +0100

    added dropdown and intent to main

--- app/src/main/java/com/example/practica05/MainActivity.java
+++ app/src/main/java/com/example/practica05/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         demana.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Activity_Hamburguesas.class);
+                Intent intent = new Intent(MainActivity.this, Activity_Hamburguesas.class   );
                 startActivity(intent);
             }
         });
